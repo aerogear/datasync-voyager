67f5dbaa04f3bc4b4656133bbdabe314c321e241
Dara Hayes <dara.hayes@redhat.com>
2018-11-28 11:21:28 +0000

    fix: comment the SecurityService interface

--- packages/apollo-voyager-server/src/security/SecurityService.ts
+++ packages/apollo-voyager-server/src/security/SecurityService.ts
@@ -8,7 +8,30 @@ import { Router } from 'express'
  * with something like Keycloak, Auth0, Google Services etc.
  */
 export interface SecurityService {
+  /**
+   * getSchemaDirectives should return an object containing directive
+   * implementations. e.g. @isAuthenticated, @hasRole
+   */
   getSchemaDirectives (): any
+
+  /**
+   * getAuthContextProvider returns the security service's AuthContextProvider
+   * The AuthContextProvider is responsible for adding the appropriate
+   * Auth related info from the request into the the GraphQL context
+   * Inside resolve functions. Example: context.auth.user
+   */
   getAuthContextProvider (): any
+
+  /**
+   * 
+   * @param expressApp the express router
+   * @param options any additional options necessary
+   * 
+   * applyAuthMiddleware should perform any setup needed to add authentication
+   * at the http serer layer. E.g. adding a passport or keycloak middleware to express.
+   * This is very connect/express oriented right now.
+   * It would be great if we could add more flexibility to support other frameworks
+   * Like hapi, koa, fastify, etc.
+   */
   applyAuthMiddleware (expressApp: Router, options: any): void
 }
