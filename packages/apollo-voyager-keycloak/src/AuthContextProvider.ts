9e3de7ba87708a82615b9eec70a91b9d06f0e601
Dara Hayes <dara.hayes@redhat.com>
2018-12-13 11:42:26 +0000

    fix: lint issues

--- packages/apollo-voyager-keycloak/src/AuthContextProvider.ts
+++ packages/apollo-voyager-keycloak/src/AuthContextProvider.ts
@@ -1,6 +1,6 @@
+import { AuthContextProvider } from '@aerogear/apollo-voyager-server'
 import { Request } from 'express'
 import { IncomingMessage } from 'http'
-import { AuthContextProvider } from '@aerogear/apollo-voyager-server';
 export class KeycloakAuthContextProvider implements AuthContextProvider {
   public readonly request: any
   public readonly accessToken: any
@@ -16,12 +16,12 @@ export class KeycloakAuthContextProvider implements AuthContextProvider {
     return this.authenticated
   }
 
-  public hasRole(role: string): boolean {
+  public hasRole (role: string): boolean {
     return this.isAuthenticated() && this.accessToken.hasRole(role)
   }
 
   // TODO where can we get this from?? The accessToken is kind of like a user but not exactly.
-  public getUser() {
+  public getUser () {
     return null
   }
 }
