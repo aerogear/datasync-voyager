8c037c6b5e2427458d021ee4b6b041b99bdeaf1e
Daniel Passos <daniel@passos.me>
2018-12-13 17:43:19 -0200

    fix: Add unit tests

---
+++ packages/apollo-voyager-offline/test/VersionedObjectState.test.ts
@@ -0,0 +1,35 @@
+import { expect } from 'chai'
+import 'mocha'
+import { VersionedObjectState } from '../src'
+
+describe('Versioned ObjectState', () => {
+
+  it('With conflict', () => {
+
+    const objectState = new VersionedObjectState()
+    const serverData = {name: 'AeroGear', version: 1}
+    const clientData = {name: 'Red Hat', version: 2}
+
+    expect(objectState.hasConflict(serverData, clientData)).eq(true)
+  })
+
+  it('Without conflict', () => {
+
+    const objectState = new VersionedObjectState()
+    const serverData = {name: 'AeroGear', version: 1}
+    const clientData = {name: 'AeroGear', version: 1}
+
+    expect(objectState.hasConflict(serverData, clientData)).eq(false)
+  })
+
+  it('Next stage ', () => {
+
+    const serverData = {name: 'AeroGear', version: 1}
+
+    const objectState = new VersionedObjectState()
+    objectState.next(serverData)
+
+    expect(serverData.version).eq(2)
+  })
+
+})
