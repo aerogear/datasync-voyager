ac8080116185ea6402069695c12f0b9a944ed15e
Dara Hayes <dara.hayes@redhat.com>
2018-12-13 17:37:57 +0000

    fix: add unit tests for ApolloVoyagerContextProvider

--- packages/apollo-voyager-server/src/context/ApolloVoyagerContextProvider.test.ts
+++ packages/apollo-voyager-server/src/context/ApolloVoyagerContextProvider.test.ts
@@ -1,3 +1,232 @@
 import test from 'ava'
 import { ApolloVoyagerContextProvider } from './ApolloVoyagerContextProvider'
+import { DefaultVoyagerConfig } from '../config/DefaultVoyagerConfig'
+import { DefaultAuthContextProvider } from '../security/DefaultSecurityService'
+import { SecurityService } from '../security/SecurityService'
+import { AuthContextProvider } from '../security/AuthContextProvider';
 
+test('DefaultVoyagerConfig will result in DefaultSecurityService inside the context', async (t) => {
+  const securityService = new DefaultVoyagerConfig().securityService
+  const contextProvider = new ApolloVoyagerContextProvider({ securityService })
+
+  const dummyRequest = {
+    method: 'GET',
+    url: '/graphql'
+  }
+  
+  const contextFn = contextProvider.getContext()
+  const context = await contextFn({ req: dummyRequest })
+  
+  const expectedContext = { request: dummyRequest, auth: new DefaultAuthContextProvider() }
+  t.deepEqual(context, expectedContext)
+})
+
+test('Passing a custom security service will result in that service being inside the context', async (t) => {
+  class CustomAuthContextProvider implements AuthContextProvider {
+    isAuthenticated() {
+      return false
+    }
+    hasRole() {
+      return false
+    }
+    getUser() {}
+  }
+
+  class CustomSecurityService implements SecurityService {
+    getSchemaDirectives() {}
+    applyAuthMiddleware() {}
+    getAuthContextProvider() {
+      return CustomAuthContextProvider
+    }
+  }
+
+  const securityService = new CustomSecurityService()
+  const contextProvider = new ApolloVoyagerContextProvider({ securityService })
+
+  const contextFn = contextProvider.getContext()
+  const context = await contextFn({})
+
+  t.truthy(context.auth)
+  t.truthy(context.auth instanceof CustomAuthContextProvider)
+})
+
+test('plain context objects are supported and merged into the resulting context', async (t) => {
+  const securityService = new DefaultVoyagerConfig().securityService
+  
+  const userContext = {
+    some: 'value',
+    another: 'property'
+  }
+
+  const contextProvider = new ApolloVoyagerContextProvider({ securityService, userContext })
+
+  const contextFn = contextProvider.getContext()
+  const context = await contextFn({})
+
+  t.truthy(context.auth)
+  t.truthy(context.auth instanceof DefaultAuthContextProvider)
+  t.is(context.some, 'value')
+  t.is(context.another, 'property')
+})
+
+test('context functions are supported and the resulting object is merged into the resulting context', async (t) => {
+  const securityService = new DefaultVoyagerConfig().securityService
+
+  const userContext = function () {
+    return {
+      some: 'value',
+      another: 'property'
+    }
+  }
+
+  const contextProvider = new ApolloVoyagerContextProvider({ securityService, userContext })
+
+  const contextFn = contextProvider.getContext()
+  const context = await contextFn({})
+
+  t.truthy(context.auth)
+  t.truthy(context.auth instanceof DefaultAuthContextProvider)
+  t.is(context.some, 'value')
+  t.is(context.another, 'property')
+})
+
+test('async context functions are supported and the resulting object is merged into the resulting context', async (t) => {
+  const securityService = new DefaultVoyagerConfig().securityService
+
+  const userContext = async function () {
+    return {
+      some: 'value',
+      another: 'property'
+    }
+  }
+
+  const contextProvider = new ApolloVoyagerContextProvider({ securityService, userContext })
+
+  const contextFn = contextProvider.getContext()
+  const context = await contextFn({})
+
+  t.truthy(context.auth)
+  t.truthy(context.auth instanceof DefaultAuthContextProvider)
+  t.is(context.some, 'value')
+  t.is(context.another, 'property')
+})
+
+test('context Promises are supported and the resolved object is merged into the resulting context', async (t) => {
+  const securityService = new DefaultVoyagerConfig().securityService
+
+  const userContext = new Promise((resolve, reject) => {
+    resolve({
+      some: 'value',
+      another: 'property'
+    })
+  })
+
+  const contextProvider = new ApolloVoyagerContextProvider({ securityService, userContext })
+
+  const contextFn = contextProvider.getContext()
+  const context = await contextFn({})
+
+  t.truthy(context.auth)
+  t.truthy(context.auth instanceof DefaultAuthContextProvider)
+  t.is(context.some, 'value')
+  t.is(context.another, 'property')
+})
+
+test('if the user context is not an object or function it will not be included', async (t) => {
+  const securityService = new DefaultVoyagerConfig().securityService
+
+  const userContext = 'not an object'
+
+  const contextProvider = new ApolloVoyagerContextProvider({ securityService, userContext })
+
+  const contextFn = contextProvider.getContext()
+
+  const dummyRequest = {
+    method: 'GET',
+    url: '/graphql'
+  }
+
+  const context = await contextFn({ req: dummyRequest })
+
+  const expectedContext = { request: dummyRequest, auth: new DefaultAuthContextProvider() }
+  t.deepEqual(context, expectedContext)
+})
+
+test('user context properties cannot override ones provided by Voyager', async (t) => {
+  const securityService = new DefaultVoyagerConfig().securityService
+  
+  const userContext = {
+    auth: 'my custom auth',
+    request: 'my custom request'
+  }
+
+  const contextProvider = new ApolloVoyagerContextProvider({ securityService, userContext })
+
+  const dummyRequest = {
+    method: 'GET',
+    url: '/graphql'
+  }
+
+  const contextFn = contextProvider.getContext()
+  const context = await contextFn({ req: dummyRequest })
+
+  t.not(context.auth, userContext.auth)
+  t.not(context.request, userContext)
+  t.truthy(context.auth instanceof DefaultAuthContextProvider)
+  t.deepEqual(context.request, dummyRequest)
+})
+
+test('if the user context function returns a null or undefined value, just the default context will be present', async (t) => {
+  const securityService = new DefaultVoyagerConfig().securityService
+
+  const userContext = function () {
+    return null
+  }
+
+  const contextProvider = new ApolloVoyagerContextProvider({ securityService, userContext })
+
+  const dummyRequest = {
+    method: 'GET',
+    url: '/graphql'
+  }
+
+  const contextFn = contextProvider.getContext()
+  const context = await contextFn({ req: dummyRequest })
+
+  const expectedContext = { request: dummyRequest, auth: new DefaultAuthContextProvider() }
+  t.deepEqual(context, expectedContext)
+})
+
+test('getContext throws when userContext function throws', async (t) => {
+  const securityService = new DefaultVoyagerConfig().securityService
+
+  const errorMsg = 'error in user context'
+  const userContext = function () {
+    throw new Error(errorMsg)
+  }
+
+  const contextProvider = new ApolloVoyagerContextProvider({ securityService, userContext })
+
+  const contextFn = contextProvider.getContext()
+
+  await t.throwsAsync(async () => {
+    await contextFn({})
+  }, errorMsg)
+})
+
+test('getContext throws when userContext Promise rejects', async (t) => {
+  const securityService = new DefaultVoyagerConfig().securityService
+
+  const errorMsg = 'error in user context'
+  const userContext = new Promise((resolve, reject) => {
+    reject(new Error(errorMsg))
+  })
+
+  const contextProvider = new ApolloVoyagerContextProvider({ securityService, userContext })
+
+  const contextFn = contextProvider.getContext()
+
+  await t.throwsAsync(async () => {
+    await contextFn({})
+  }, errorMsg)
+})
