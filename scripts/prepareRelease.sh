1cbaee828f7782cd84ada98be1605bd29f079843
Ali Ok <aliok@apache.org>
2018-12-25 14:26:44 +0300

    Release 0.1.4

---
+++ scripts/prepareRelease.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Preparing release"
+
+rm -Rf node_modules
+npm install
+npm run bootstrap
+npm run compile
+npm run test
+npm run lint
+
+# don't run in CI
+if [ ! "$CI" = true ]; then
+  ./node_modules/.bin/lerna publish --skip-git --force-publish=* --skip-npm
+fi
+
+echo "Repository is ready for release."
+
