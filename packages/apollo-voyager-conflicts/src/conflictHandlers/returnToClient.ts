dbab14c343c2da8629d0b035456fb5da61e94720
Wojciech Trocki <wtrocki@redhat.com>
2019-01-02 11:56:12 +0000

    feat: Return to Client resolution strategy

---
+++ packages/apollo-voyager-conflicts/src/conflictHandlers/returnToClient.ts
@@ -0,0 +1,25 @@
+import debug from 'debug'
+import { ConflictResolutionHandler } from '../api/ConflictHandler'
+import { ObjectConflictError } from '../api/ObjectConflictError'
+import { ObjectState } from '../api/ObjectState'
+import { ObjectStateData } from '../api/ObjectStateData'
+import { CONFLICT_LOGGER } from '../constants'
+
+export const logger = debug(CONFLICT_LOGGER)
+
+/**
+ * @param currentRecord the object state that the server knows about
+ * @param client the object state that the client knows about
+ */
+export const returnToClient: ConflictResolutionHandler =
+  (serverState: ObjectStateData, clientState: ObjectStateData) => {
+    logger(`Conflict detected.
+    Sending data to resolve conflict on client
+    Server: ${serverState} client: ${clientState}`)
+
+    throw new ObjectConflictError({
+      clientData: clientState,
+      serverData: serverState,
+      resolvedOnServer: false
+    })
+  }
