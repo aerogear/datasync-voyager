cee86b95e8089e9ddd888cabe9d102ee9986b15c
Wojciech Trocki <wtrocki@redhat.com>
2019-01-02 10:23:03 +0000

    fix: Build abstraction for conflict error
    Motivation:
    We do not need to have flag if we return some structured data to client that can be recognized as error. This will simplify implementation. Error will be conflict specific as there are no other examples or error that can justify generalized type. All new error should have their own specialized types. Having dedicated error for conflict allows us to strongly type response object.

---
+++ packages/apollo-voyager-offline/src/api/ObjectConflictError.ts
@@ -0,0 +1,42 @@
+import { GraphQLError } from 'graphql'
+import { ObjectStateData } from '../ObjectStateData'
+
+/**
+ * Data returned back to client wrapped in error objects.
+ * Client side link highly depend on this data to notify client about conflict
+ * and eventually perform client side conflict resolution
+ */
+export interface ConflictData {
+
+  /**
+   * Data that was modified on the server.
+   * Source of the conflict with additional fields that changed over the time.
+   */
+  serverData: ObjectStateData
+
+  /**
+   * Original data that was sent from client and cannot be applied because of conflict
+   */
+  clientData?: ObjectStateData
+
+  /**
+   * Flag used to inform client that conflict was already resolved on the server
+   * and no further processing is needed. When flag is true `serverData` field will contain
+   * resolved information. If value is false client will need to resolve conflict on their side
+   * and both `serverData` and `clientData` will be available
+   */
+  resolvedOnServer: boolean
+}
+
+/**
+ * Conflict error that is being returned when server
+ * Error specific to Voyager framework
+ */
+export class DataConflictError extends GraphQLError {
+  public conflictInfo: any
+
+  constructor(data: ConflictData) {
+    super('VoyagerConflict')
+    this.conflictInfo = data
+  }
+}
