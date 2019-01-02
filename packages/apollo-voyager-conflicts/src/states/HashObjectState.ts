06f0ce991e104490b8f436a96b14b9b0453fcf65
Wojciech Trocki <wtrocki@redhat.com>
2019-01-02 11:54:54 +0000

    fix: add hash state manager

---
+++ packages/apollo-voyager-conflicts/src/states/HashObjectState.ts
@@ -0,0 +1,40 @@
+import * as debug from 'debug'
+import { ObjectState } from '../api/ObjectState'
+import { CONFLICT_LOGGER } from '../constants'
+
+/**
+ * Object state manager using a hash field on object
+ * Detects conflicts and allows moving to next state using the hash field of the object
+ *
+ * HashObjectState requires GraphQL types to contain hash field.
+ * For example:
+ *
+ * type User {
+ *   id: ID!
+ *   hash: String
+ * }
+ */
+export class HashObjectState implements ObjectState {
+  private logger = debug.default(CONFLICT_LOGGER)
+  private hash: (object: any) => string
+
+  constructor(hashImpl: (object: any) => string) {
+    this.hash = hashImpl
+  }
+
+  public hasConflict(serverData: any, clientData: any) {
+    if (serverData.hash && clientData.hash) {
+      if (serverData.hash !== clientData.hash) {
+        this.logger(`Conflict when saving data. current: ${serverData}, client: ${clientData}`)
+        return true
+      }
+    }
+    return false
+  }
+
+  public next = (currentObjectState: ConflictResolutionData): ConflictResolutionData => {
+    this.logger(`Moving object to next state, ${currentObjectState}`)
+    currentObjectState.hash = this.hash(currentObjectState)
+    return currentObjectState
+  }
+}
