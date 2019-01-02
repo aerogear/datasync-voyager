22d36158a4d0a79a28da58d3c8de0e6cb9e4c3a5
Wojciech Trocki <wtrocki@redhat.com>
2019-01-02 10:24:41 +0000

    feat: Introduce type for ObjectStateData
    ObjectStateData is an type that is going to map data object passed to different parts of the API. See documentation for more info

--- packages/apollo-voyager-offline/src/constants.ts
+++ packages/apollo-voyager-offline/src/constants.ts
@@ -1,7 +1,2 @@
-/**
- * Config error type used to determine if conflict happened
- */
-export const CONFLICT_ERROR = 'AgSync:DataConflict'
-
 // Feature loggers
 export const CONFLICT_LOGGER = 'AeroGearSync:Conflict'
