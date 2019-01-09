dacd0457fc3e97ffab45f46a0a839f19f4dfd972
Dara Hayes <dara.hayes@redhat.com>
2019-01-09 14:13:15 +0000

    fix: refactor strategies

--- packages/apollo-voyager-conflicts/src/strategies/index.ts
+++ packages/apollo-voyager-conflicts/src/strategies/index.ts
@@ -1,17 +1,18 @@
 import { ConflictResolutionStrategy } from '../api/ConflictResolutionStrategy'
 import { ObjectStateData } from '../api/ObjectStateData'
 
-export interface StrategyMap {[key: string]: ConflictResolutionStrategy}
-
-function clientWins (serverState: ObjectStateData, clientState: ObjectStateData) {
+function clientWinsstrategy (serverState: ObjectStateData, clientState: ObjectStateData) {
   return clientState
 }
 
-function serverWins (serverState: ObjectStateData, clientState: ObjectStateData) {
+function serverWinsStrategy (serverState: ObjectStateData, clientState: ObjectStateData) {
   return serverState
 }
 
-export const strategies: StrategyMap = {
+const clientWins: ConflictResolutionStrategy = clientWinsstrategy
+const serverWins: ConflictResolutionStrategy = serverWinsStrategy
+
+export const strategies = {
   clientWins,
   serverWins
 }
