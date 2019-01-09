ab88679adfab08c956b49fa00faf0de8be743655
Dara Hayes <dara.hayes@redhat.com>
2019-01-09 12:57:49 +0000

    feat: add default clientWins and serverWins strategies

--- packages/apollo-voyager-conflicts/src/index.ts
+++ packages/apollo-voyager-conflicts/src/index.ts
@@ -6,6 +6,10 @@ export * from './api/ObjectStateData'
 // State implementations
 export * from './states/VersionedObjectState'
 export * from './states/HashObjectState'
+
+// Strategy implementations
+export * from './strategies'
+
 // Default API state handler
 export { versionStateHandler as conflictHandler }
   from './states/VersionedObjectState'
