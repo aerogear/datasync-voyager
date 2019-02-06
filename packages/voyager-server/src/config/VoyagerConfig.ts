4ce2fc4be1c60f3658419031eed6aec3122a7451
Dara Hayes <dara.hayes@redhat.com>
2019-02-06 16:29:19 +0000

    fix: refactor config to user facing VoyagerConfig and internal CompleteVoyagerConfig

--- packages/voyager-server/src/config/VoyagerConfig.ts
+++ packages/voyager-server/src/config/VoyagerConfig.ts
@@ -4,24 +4,25 @@ import { SecurityService } from '@aerogear/voyager-keycloak'
 import { Metrics } from '@aerogear/voyager-metrics'
 
 export interface VoyagerConfig {
+  
+  /**
+  * Enables Authentication and Authorization support
+  */
+  securityService?: SecurityService | null
 
   /**
-   * Enables Authentication and Authorization support
-   */
+  * Enables metrics feature
+  */
+  metrics?: Metrics | null
+  
+  /**
+  * Enables audit logging feature
+  */
+  auditLogger?: AuditLogger | null
+}
+
+export interface CompleteVoyagerConfig {
   securityService: SecurityService
-  /**
-   * Enables metrics feature
-   */
-  metrics?: Metrics
-
-  /**
-   * Enables audit logging feature
-   */
-  auditLogger?: AuditLogger
-
-  /**
-   * Enables to customize default approach for conflict handling.
-   * By default it will use VersionedObjectState
-   */
-  conflict?: ObjectState
+  metrics: Metrics
+  auditLogger: AuditLogger
 }
