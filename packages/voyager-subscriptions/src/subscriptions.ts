73d3af55ae21acd232c8f41ba16717314d46ff66
Stephen Coady <scoady@redhat.com>
2019-02-19 12:36:34 +0000

    feat: moved to a class. added push functions

--- packages/voyager-subscriptions/src/subscriptions.ts
+++ packages/voyager-subscriptions/src/subscriptions.ts
@@ -1,18 +1,66 @@
-import { execute, subscribe } from 'graphql'
+import { execute, subscribe, GraphQLSchema } from 'graphql'
+import { PubSub } from 'apollo-server'
 import { SubscriptionServer, ConnectionParams } from 'subscriptions-transport-ws'
 import { KeycloakSecurityService } from '@aerogear/voyager-keycloak'
+import { agSender } from 'unifiedpush-node-sender'
+import { fromPromise } from 'apollo-link'
 
-export function subscriptionServer(keycloakService: KeycloakSecurityService) {
+export class Sync {
+
+private pushClient: any = null
+private pubSub: any = null
+
+public subscriptionServer(keycloakService: KeycloakSecurityService, schema: GraphQLSchema, httpServer: any) {
+  this.pubSub = new PubSub()
   return new SubscriptionServer({
     execute,
     subscribe,
     onConnect: async (connectionParams: ConnectionParams) => {
-      return await keycloakService.validateToken(connectionParams)
+      return await keycloakService.validateToken(connectionParams.token)
     },
-    schema: apolloServer.schema
+    schema
   }, {
       server: httpServer,
       path: '/graphql'
-    });
+    })
+
+}
+
+public setUpPushClient(config: any) {
+  if (config) {
+    const pushService = agSender(config.pushConfig)
+    this.pushClient = pushService.then((client: any) => {
+      return client
+    })
+  }
+}
+
+public getPushClient() {
+  return this.pushClient
+}
+
+public publish(actionType: string, data: any, push: boolean) {
+  if (push) {
+    const message = {
+      alert: `${actionType} ${data.title}`
+    }
+    const options = {
+      config: {
+        ttl: 3600
+      }
+    }
+    this.pushClient.sender.send(message, options).then((response: any) => {
+      console.log('Notification sent, response received ', response)
+    })
+  }
+  this.pubSub.publish(TASKS_SUBSCRIPTION_KEY, {
+    tasks: {
+      action: actionType,
+      task: data
+    }
+  })
+}
+}
+
 
 }
