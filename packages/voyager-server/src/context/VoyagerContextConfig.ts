6d95dc38a8240da7fdebcc91f30ada66c2553398
Dara Hayes <dara.hayes@redhat.com>
2019-02-27 12:21:46 +0000

    fix: ensure correct typings are used in VoyagerContextConfig

--- packages/voyager-server/src/context/VoyagerContextConfig.ts
+++ packages/voyager-server/src/context/VoyagerContextConfig.ts
@@ -1,9 +1,8 @@
-import { auditLog } from '@aerogear/voyager-audit'
-import { ObjectState } from '@aerogear/voyager-conflicts'
+import { AuditLogger } from '@aerogear/voyager-audit'
 import { SecurityService } from '@aerogear/voyager-keycloak'
 
 export interface VoyagerContextProviderConfig {
   userContext?: any,
   securityService: SecurityService,
-  auditLogger: any,
+  auditLogger: AuditLogger,
 }
