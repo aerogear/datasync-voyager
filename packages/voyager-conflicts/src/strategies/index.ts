8a4636f46e43572c876193523cbc70ff1e183897
Wojciech Trocki <wtrocki@redhat.com>
2019-02-28 11:05:26 +0000

    fix: AEROGEAR-8614 remove invalid strategy

--- packages/voyager-conflicts/src/strategies/index.ts
+++ packages/voyager-conflicts/src/strategies/index.ts
@@ -1,18 +1,12 @@
 import { ConflictResolutionStrategy } from '../api/ConflictResolutionStrategy'
 import { ObjectStateData } from '../api/ObjectStateData'
 
-function clientWinsstrategy (serverState: ObjectStateData, clientState: ObjectStateData) {
+function clientWinsStrategy (serverState: ObjectStateData, clientState: ObjectStateData) {
   return clientState
 }
 
-function serverWinsStrategy (serverState: ObjectStateData, clientState: ObjectStateData) {
-  return serverState
-}
-
-const clientWins: ConflictResolutionStrategy = clientWinsstrategy
-const serverWins: ConflictResolutionStrategy = serverWinsStrategy
+const clientWins: ConflictResolutionStrategy = clientWinsStrategy
 
 export const strategies = {
-  clientWins,
-  serverWins
+  clientWins
 }
