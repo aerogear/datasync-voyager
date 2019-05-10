22f1339f0b44015a6aeda1c7b3191c930a07aa4b
Dara Hayes <dara.hayes@redhat.com>
2019-05-10 16:50:52 +0100

    chore: comments in VoyagerSubscriptionContextProvider

--- packages/voyager-subscriptions/src/VoyagerSubscriptionContextProvider.ts
+++ packages/voyager-subscriptions/src/VoyagerSubscriptionContextProvider.ts
@@ -6,15 +6,17 @@ import { VoyagerSubscriptionServerOptions } from './api'
 type OnConnectFunction = (connectionParams: any, webSocket: any, context: any) => any
 
 /**
- * The VoyagerContextProvider will be used to extend the GraphQL context
- * With info from the various Voyager framework components
- * e.g. Security, Audit Logging, client info.
+ * VoyagerSubscriptionContextProvider's purpose is to provide an
+ * 'onConnect' function that allows developers to supply their own onConnect
+ * the way they would with plain Apollo. However this onConnect will be combined
+ * with our own onConnect which may do some things such as authentication/authorization
+ * The results from our onConnect and the developer's onConnect functions are combined together
+ * and are made available in the GraphQL context inside subscription resolvers
+ * 
  */
 export class VoyagerSubscriptionContextProvider {
 
-  // public config: VoyagerContextProviderConfig
 
-  // public authContextProvider: any
   public userOnConnectFunction: any
   public userOnConnectObject: any
   public securityService: SecurityService
@@ -34,11 +36,15 @@ export class VoyagerSubscriptionContextProvider {
   }
 
   /**
-   * returns the context object or function that should be passed into new ApolloServer()
+   * returns onConnect function that will be passed into new SubscriptionServer()
+   * the returned onConnect function combines the contexts (results) of the
+   * developer onConnect and a default onConnect provided here.
+   * This allows us to inject additional integrations into subscriptions such as 
+   * auth, metrics, auditLogging, etc
    */
   public getOnConnectFunction(): OnConnectFunction {
     const subscriptionContextProvider = this
-    return async function combineContexts(connectionParams: any, webSocket: any, context: any): Promise<object> {
+    return async function onConnect(connectionParams: any, webSocket: any, context: any): Promise<object> {
 
       const defaultContext = await subscriptionContextProvider.getDefaultContext(connectionParams, webSocket, context)
       let userContext = subscriptionContextProvider.buildUserContext(connectionParams, webSocket, context)
@@ -51,6 +57,16 @@ export class VoyagerSubscriptionContextProvider {
     }
   }
 
+  /**
+   * 
+   * returns the onConnect value provided by the developer
+   * if the user provided onConnect is a function
+   * the result of that function is returned
+   * 
+   * @param connectionParams 
+   * @param webSocket 
+   * @param context 
+   */
   private buildUserContext(connectionParams: any, webSocket: any, context: any): object {
     if (this.userOnConnectFunction) {
       return this.userOnConnectFunction(connectionParams, webSocket, context)
@@ -58,6 +74,17 @@ export class VoyagerSubscriptionContextProvider {
     return this.userOnConnectObject
   }
 
+  /**
+   * 
+   * returns any framework specific things we want to add
+   * into the graphql context for subscriptions
+   * right now it's just auth information provided by the given security service implementation
+   * Could also be extended to include audit logging, metrics, etc.
+   * 
+   * @param connectionParams 
+   * @param webSocket 
+   * @param context 
+   */
   private async getDefaultContext(connectionParams: any, webSocket: any, context: any): Promise<any> {
     if (this.securityService) {
       const auth = await this.securityService.onSubscriptionConnect(connectionParams, webSocket, context)
