742c50406ab203bfb8397ad45835323622425b93
Wojciech Trocki <wtrocki@redhat.com>
2019-07-05 16:36:20 +0100

    fix: Use upstream package

--- packages/voyager-conflicts/src/index.ts
+++ packages/voyager-conflicts/src/index.ts
@@ -1,15 +1,2 @@
 // Conflict api
-export * from './api/ObjectState'
-export * from './api/ConflictResolution'
-export * from './api/ObjectStateData'
-
-// State implementations
-export * from './states/VersionedObjectState'
-export * from './states/HashObjectState'
-
-// Strategy implementations
-export * from './strategies'
-
-// Default API state handler
-export { versionStateHandler as conflictHandler }
-  from './states/VersionedObjectState'
+export * from 'offix-server-conflicts'
