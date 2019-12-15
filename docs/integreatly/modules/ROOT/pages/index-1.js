7c4b9d3957fde3d415c58785b7920de35b29d0a8
Joan Edwards <joaedwar@redhat.com>
2019-12-15 22:24:43 +0000

    Add index files back

---
+++ docs/integreatly/modules/ROOT/pages/index-1.js
@@ -0,0 +1,33 @@
+const express = require('express')
+//Include our server libraries
+const { VoyagerServer, gql } = require('@aerogear/voyager-server')
+
+//Provide your graphql schema
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`
+
+//Create the resolvers for your schema
+const resolvers = {
+  Query: {
+    hello: (obj, args, context, info) => {
+      return `Hello world`
+    }
+  }
+}
+
+//Initialize the library with your Graphql information
+const apolloServer = VoyagerServer({
+  typeDefs,
+  resolvers
+})
+
+//Connect the server to express
+const app = express()
+apolloServer.applyMiddleware({ app })
+
+app.listen(4000, () =>
+  console.log(`🚀 Server ready at http://localhost:4000/graphql`)
+)
